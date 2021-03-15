1f20a9c865304b6e52f77366c5d08baf4d814697
Nicolas Yang <nicolas.yang.1@gmail.com>
2021-03-15 20:54:59 +0100

    correct signin btn

--- js/navBar.js
+++ js/navBar.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     let menuList = document.querySelectorAll("header nav ul li a");
 
+    let regexClickSignIn = /index.html#footer$/
+
+    regexClickSignIn.test(document.location) ? menuList[2].classList.add("clickDark") : false;
 
     for (let i = 0; i < menuList.length; i++) {
         menuList[i].addEventListener("click", () => {
