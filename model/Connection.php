a2d9932a1c9c2ebf067f71ba46f844271a799be2
Nicolas Yang <nicolas.yang.1@gmail.com>
2021-03-22 09:53:42 +0100

    Update connection part

--- model/Connection.php
+++ model/Connection.php
@@ -5,32 +5,30 @@ require_once('Manager.php');
 class Connection extends Manager
 {
 
-    function checkLoginPassword($inputMail, $inputPassword)
+    function checkLoginPassword($inputLogin, $inputPassword)
     {
         $db = $this->dbConnect();
-        $req = $db->query("SELECT * FROM user_mail WHERE mail = '" . $inputMail . "'");
+        $req = $db->query("SELECT * FROM user_mail WHERE mail = '" . $inputLogin . "'OR nom = '" . $inputLogin . "'");
 
         $data = $req->fetch();
         $isPasswordCorrect = password_verify($inputPassword, $data['password']);
 
-        if (!$data) {
+        if (!isset($inputLogin, $inputPassword)) {
             return '<h2>Veuillez remplir tous les champs!</h2>';
+        } elseif ($isPasswordCorrect) {
+            session_start();
+            $_SESSION['id'] = $data['id'];
+            $_SESSION['login'] = $inputLogin;
+            return '<h2>Bonjour ' . $data['prenom'] . '. Vous êtes connecté !</h2>';
         } else {
-            if ($isPasswordCorrect) {
-                session_start();
-                $_SESSION['id'] = $data['id'];
-                $_SESSION['mail'] = $inputMail;
-                return '<h2>Bonjour ' . $data['prenom'] . '. Vous êtes connecté !</h2>';
-            } else {
-                return '<h2>Erreur identifiant ou mot de passe !</h2>';
-            }
+            return '<h2>Erreur identifiant ou mot de passe !</h2>';
         }
     }
 
     function logIn()
     {
-        if (isset($_POST['mail'], $_POST['password'])) {
-            return  $this->checkLoginPassword(htmlspecialchars($_POST['mail']), htmlspecialchars($_POST['password']));
+        if (isset($_POST['login'], $_POST['password'])) {
+            return  $this->checkLoginPassword(htmlspecialchars($_POST['login']), htmlspecialchars($_POST['password']));
         } else {
             return "<h2>Bienvenue dans votre espace.</h2>";
         }
--- view/connexionView.php
+++ view/connexionView.php
@@ -3,12 +3,12 @@ ob_start();
 ?>
 <main>
     <section class="login-page">
-    <?= $connectUser->logIn() ?>
+        <?= $connectUser->logIn() ?>
         <form action="index.php?action=logIn" method="post">
             <fieldset>
                 <legend>Connectez vous à votre compte</legend>
-                <label for="mail">Mail *</label>
-                <input type="email" name="mail" id="mail" placeholder="Votre mail" aria-required="true">
+                <label for="login">Mail ou login*</label>
+                <input type="text" name="login" id="login" placeholder="Votre mail" aria-required="true">
                 <label for="password">Mot de passe *</label>
                 <input type="password" name="password" id="password" placeholder="Votre mot de passe" aria-required="true">
                 <input type="submit" value="Connection à votre compte">
